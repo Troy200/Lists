d44d8cc75e12c96e3ab72bae67b17cdebfe167b1
Troy200 <kuriyanthomas27@gmail.com>
2025-02-05 17:54:31 +0100

    Add files via upload

---
+++ Revision.py
@@ -0,0 +1,13 @@
+sports=["foorball","basketball"]
+print (sports)
+sports.append("baseball")
+print (sports)
+sports.insert(1,"hockey")
+print (sports)
+sports.remove("basketball")
+print (sports)
+sports.pop(1)
+print (sports)
+sports[0]="football"
+print (sports)
+
